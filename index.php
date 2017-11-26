6837ff5b13ed83ebdc9b8a9c8da1ef9ff4680323
SONY <SONY@192.168.0.10>
2017-11-26 14:03:01 -0500

    comit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+    <link rel="stylesheet" type="text/css" href="Common/css/bootstrap.min.css" media="screen" />
+    
+</head>
+<body>
+    <div class="row">
+        <div class="col-sm-3">izq</div>
+        <div class="col-sm-6">
+            <form class="form-signin">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                  </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+        </div>
+        <div class="col-sm-3">der</div>        
+    </div>
+</body>
+<script src="Common/js/bootstrap.min.js"></script>
+
+</html>
