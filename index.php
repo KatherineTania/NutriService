fa3df09892954f14a75d0d3d9cb862ff89bbc414
SONY <SONY@192.168.0.17>
2017-11-27 15:47:29 -0500

    Prueba index

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
         <div class="col-sm-3">izq</div>
         <div class="col-sm-6">
             <form class="form-signin">
-                <h2 class="form-signin-heading">Please sign in</h2>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <h2 class="form-signin-heading">Inicio de Sesion</h2>
+                <label for="inputEmail" class="sr-only">Usuario: </label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+                <label for="inputPassword" class="sr-only">Clave: </label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Clave" required>
                 <div class="checkbox">
                   <label>
                     <input type="checkbox" value="remember-me"> Remember me
                   </label>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
             </form>
         </div>
         <div class="col-sm-3">der</div>        
